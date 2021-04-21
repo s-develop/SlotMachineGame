ef716f0f475a1ac160fc432ecc424d188ec34125
Sergei Matveev <sgsq@ya.ru>
2021-04-21 11:42:08 +0400

    Initial Commit

---
+++ Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Sergei on 21.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
